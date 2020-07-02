167107ce5422625adfaa8c468c236183d0e5fc4d
iva mudronja <iva.pilizota@gmail.com>
2020-07-02 22:45:58 +0200

    add search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :cocktails, only: [:new, :index, :show, :create, :destroy] do
+  resources :cocktails, only: [:new, :index, :show, :create] do
     resources :doses, only: [:new, :create]
     resources :reviews, only: [:new, :create]
   end
